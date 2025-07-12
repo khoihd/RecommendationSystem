8aebdc93d6e5ae1fd2e63156a7909c80f8462c3e
Khoi Hoang <khoi.hoang@wustl.edu>
2025-07-12 14:44:07 +0700

    Update imports

--- seq2seq/__init__.py
+++ seq2seq/__init__.py
@@ -5,4 +5,4 @@ from .Seq2Seq import Seq2Seq
 from .Seq2Seq_GRU import Seq2Seq_GRU
 from .Seq2Seq_GRU_Attention import Seq2Seq_GRU_Attention
 from .Seq2Seq_GRU_Attention import DOT_PRODUCT, SCALED_DOT_PRODUCT, GENERALIZED_DOT_PRODUCT
-from .Seq2Seq_GRU_Attention import LOCAL_MONOTONIC, LOCAL_PREDICTIVE_ALIGN
+from .Seq2Seq_GRU_Attention import LOCAL_MONOTONIC_ALIGN, LOCAL_PREDICTIVE_ALIGN
