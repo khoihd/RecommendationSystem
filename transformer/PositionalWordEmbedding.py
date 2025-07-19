20598259f87ba48449737e5b2449045559208e73
Khoi Hoang <khoi.hoang@wustl.edu>
2025-07-19 16:45:15 +0700

    Add unit test for positional encoding

--- transformer/PositionalWordEmbedding.py
+++ transformer/PositionalWordEmbedding.py
@@ -1,7 +1,7 @@
+import math
 import torch
 import torch.nn as nn
 
-# TODO: Test this class
 class PositionalWordEmbedding(nn.Module):
     def __init__(self, vocab_size, dim):
         super().__init__()
@@ -18,15 +18,17 @@ class PositionalWordEmbedding(nn.Module):
             result = []
             for d in range(self.dim):
                 if d%2 == 0:
-                    emb_val = torch.sin(i / (10000**(d/self.dim)))
+                    emb_val = math.sin(i / (10000**(d/self.dim)))
                 else:
-                    emb_val = torch.cos(i / (10000**((d-1)/self.dim)))
+                    emb_val = math.cos(i / (10000**((d-1)/self.dim)))
                 result.append(emb_val)
-            return torch.tensor(result)
+            return result
         
         batch, seq_len = x.size()
         emb = []
         for i in range(seq_len):
             emb.append(pos_emb_at(i))
+        
+        emb = torch.tensor(emb)
         
         return emb.repeat(batch, seq_len, 1)    
---
+++ transformer/test_transformer.py
@@ -0,0 +1,35 @@
+import math
+import unittest
+import torch
+from PositionalWordEmbedding import PositionalWordEmbedding
+
+# Test class
+class TestPositionalEncoding(unittest.TestCase):
+    def test_positional_encoding(self):
+        batch_size = 64
+        seq_len = 100
+        vocab_size, emb_dim = 1000, 512
+
+        training_set = []
+        for batch in range(batch_size):
+            training_set.append(list(range(seq_len)))
+
+        training_set = torch.tensor(training_set)
+
+        positional_embedding = PositionalWordEmbedding(vocab_size, emb_dim)
+        emb = positional_embedding.positional_embeddding(training_set)
+
+        print(f"Running test for position embedding with batch_size={batch_size}, seq_len={seq_len}, emb_dim={emb_dim}...")
+        for batch in range(batch_size):
+            for i in range(seq_len):
+                for d in range(emb_dim):
+                    if d%2 == 0:
+                        # assert abs(emb[batch][i][d] - math.sin(i / 10000**(d/emb_dim))) < sys.float_info.epsilon
+                        assert emb[batch][i][d] == math.sin(i / 10000**(d/emb_dim))
+                    else:
+                        # assert abs(emb[batch][i][d] - math.cos(i / 10000**((d-1)/emb_dim))) < sys.float_info.epsilon
+                        assert emb[batch][i][d] == math.cos(i / 10000**((d-1)/emb_dim))
+
+# Run tests if executed directly
+if __name__ == '__main__':
+    unittest.main()
