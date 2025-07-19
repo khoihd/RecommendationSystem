cfe81f5bb4af2687e774c9745bb00a8e72575d79
Khoi Hoang <khoi.hoang@wustl.edu>
2025-07-19 20:03:30 +0700

    Update Transformer Encoder

--- transformer/Attention.py
+++ transformer/Attention.py
@@ -1,15 +1,16 @@
 import torch.nn as nn
 
 # TODO: Masked
-# TODO: Multihead
+# TODO: To parallelize attention with multi-heads
 class Attention(nn.Module):
-    def __init__(self, heads=1, masked=True):
+    def __init__(self, input_dim, att_dim, heads=1, masked=False):
         super().__init__()
         self.attention = []
+        # To parallelize attention with multi-heads
         for head in range(heads):
-            Q = nn.Linear()
-            K = nn.Linear()
-            V = nn.Linear()
+            Q = nn.Linear(input_dim, att_dim, bias=True)
+            K = nn.Linear(input_dim, att_dim, bias=True)
+            V = nn.Linear(input_dim, att_dim, bias=True)
             self.attention.append([Q, K, V])
         
     def forward(self, q, k, v):
--- transformer/TransformerEncoder.py
+++ transformer/TransformerEncoder.py
@@ -1,8 +1,34 @@
 import torch.nn as nn
+from PositionalWordEmbedding import PositionalWordEmbedding
+from AddNorm import AddNorm
+from Attention import Attention
 
 class TransformerEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, emb_dim=512, att_dim=64, att_heads=8, ffn_dim=2048, layers=6):
         super().__init__()
+        # Embedd input
+        self.embedding = PositionalWordEmbedding(vocab_size, emb_dim)
+        self.layers = layers
+
+        self.encoding_layers = []
+        for _ in range(layers):
+            self.encoding_layers.append([
+                Attention(emb_dim, att_dim, att_heads),
+                AddNorm(),
+                nn.Linear(att_dim*att_heads, ffn_dim),
+                nn.ReLU(),
+                nn.Linear(ffn_dim, att_dim*att_heads),
+                AddNorm()
+            ])
 
     def forward(self, x):
-        pass
+        x = self.embedding(x)
+        for encoder in self.encoding_layers:
+            att_x = encoder[0](x)    # attention
+            x = encoder[1](x, att_x) # add_norm
+            ffc_x = encoder[2](x)       # fcc
+            ffc_x = encoder[3](ffc_x)   # relu
+            ffc_x = encoder[4](ffc_x)   # fcc
+            x = encoder[5](x, ffc_x)    # add_norm
+        
+        return x
