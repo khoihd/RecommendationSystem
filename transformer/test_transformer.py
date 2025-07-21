11d9643335479a9cbf16eeb921d2c454035837ee
Khoi Hoang <khoi.hoang@wustl.edu>
2025-07-22 01:02:06 +0700

    Update test

--- transformer/test_transformer.py
+++ transformer/test_transformer.py
@@ -16,14 +16,12 @@ class TestPositionalEncoding(unittest.TestCase):
         np_values = np.arange(0, 2*np.pi, datapoints)
         torch_values = torch.arange(0, 2*torch.pi, datapoints)
         
-        print("Running precision test for sine and cosine functions from torch and numpy...")
-        assert torch.sin(torch_values).numpy() == np.sin(np_values)
-        assert torch.cos(torch_values).numpy() == np.cos(np_values)
-        print("Done")
+        self.assertEqual(torch.sin(torch_values).numpy(), np.sin(np_values))
+        self.assertEqual(torch.cos(torch_values).numpy(), np.cos(np_values))
     
     def test_02_positional_encoding(self):
-        batch_size = 64
-        seq_len = 100
+        batch_size = 16
+        seq_len = 50
         vocab_size, emb_dim = 1000, 512
 
         training_set = []
@@ -39,19 +37,16 @@ class TestPositionalEncoding(unittest.TestCase):
         pytorch_emb = pytorch_positional_embedding(training_set.transpose(0, 1))
         pytorch_emb = pytorch_emb.transpose(0, 1)
 
-        print(f"Running test for positional embedding with batch_size={batch_size}, seq_len={seq_len}, emb_dim={emb_dim}...")
-        print(f"pytorch_emb size:{pytorch_emb.size()}")
-        print(f"emb size:{emb.size()}")
         for batch in range(batch_size):
             for i in range(seq_len):
                 for d in range(emb_dim):
-                    assert emb[batch][i][d] == pytorch_emb[batch][i][d]
+                    self.assertEqual(emb[batch][i][d], pytorch_emb[batch][i][d])
                     if d%2 == 0:
                         emb_val = torch.sin(torch.tensor(i / 10000**(d/emb_dim))).item()
                     else:
                         emb_val = torch.cos(torch.tensor(i / 10000**((d-1)/emb_dim))).item()
                     
-                    assert abs(emb[batch][i][d] - emb_val) < TestPositionalEncoding.PRECISION
+                    self.assertAlmostEqual(emb[batch][i][d].item(), emb_val, places=4)
                         
 
 # Run tests if executed directly
