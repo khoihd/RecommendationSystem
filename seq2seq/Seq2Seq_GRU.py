2c54a548e8296a0819df38b70f44d00f3a583eb6
Khoi Hoang <khoi.hoang@wustl.edu>
2025-09-15 19:00:40 +0700

    Add a newline for better reading

--- seq2seq/Seq2Seq_GRU.py
+++ seq2seq/Seq2Seq_GRU.py
@@ -10,6 +10,7 @@ class Seq2Seq_GRU(nn.Module):
         
         if bidirectional:
             layer = layer * 2
+        
         self.decoder_emb = nn.Embedding(output_dim, output_emb_dim)
         self.decoder = nn.GRU(output_emb_dim, decoder_hidden_dim, layer, bias=True, batch_first=True, bidirectional=False)
         self.fcc = nn.Linear(decoder_hidden_dim, output_dim)
